18c6b218114895e406ee136937218c43596ef0d5
BRIDGE <abrebridge2628@gmail.com>
2026-04-11 12:21:48 +0100

    Update Android Gradle plugin and Kotlin version

--- client/android/build.gradle.kts
+++ client/android/build.gradle.kts
@@ -1,6 +1,4 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-
 plugins {
-    id("com.android.application") version "8.1.0" apply false  // or whatever version Flutter stable currently uses
-    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+    id("com.android.application") version "8.7.0" apply false   // Fr ion
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
