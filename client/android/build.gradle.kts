6f4568e4659c7ae8b3b17324216432798098cb0f
BRIDGE <abrebridge2628@gmail.com>
2026-04-11 12:11:37 +0100

    Update build.gradle.kts

--- client/android/build.gradle.kts
+++ client/android/build.gradle.kts
@@ -1,24 +1,6 @@
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
-}
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
 
-val newBuildDir: Directory =
-    rootProject.layout.buildDirectory
-        .dir("../../build")
-        .get()
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
+plugins {
+    id("com.android.application") version "8.1.0" apply false  // or whatever version Flutter stable currently uses
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
 }
