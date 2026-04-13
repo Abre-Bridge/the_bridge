a2968a2fb099e98e0b4842c852b3dd89e8860eb3
Abre Bridge <abrebridge2628@gmal.com>
2026-04-12 20:58:26 -0400

    V2

--- client/android/build.gradle.kts
+++ client/android/build.gradle.kts
@@ -1,4 +1,4 @@
 plugins {
-    id("com.android.application") version "8.7.0" apply false   // Fr ion
-    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
+    id("com.android.application") version "8.9.1" apply false
+    id("org.jetbrains.kotlin.android") version "2.2.20" apply false
 }
