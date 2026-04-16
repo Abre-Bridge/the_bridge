2281a596cc22974d4f67c0aba8ab380ce9f581c8
Abre Bridge <abrebridge2628@gmal.com>
2026-04-16 19:31:52 -0400

    Fix build error 1

--- client/android/settings.gradle.kts
+++ client/android/settings.gradle.kts
@@ -2,10 +2,13 @@ pluginManagement {
     val flutterSdkPath =
         run {
             val properties = java.util.Properties()
-            file("local.properties").inputStream().use { properties.load(it) }
-            val flutterSdkPath = properties.getProperty("flutter.sdk")
-            require(flutterSdkPath != null) { "flutter.sdk not set in local.properties" }
-            flutterSdkPath
+            val propertiesFile = file("local.properties")
+            if (propertiesFile.exists()) {
+                propertiesFile.inputStream().use { properties.load(it) }
+            }
+            val sdkPath = properties.getProperty("flutter.sdk") ?: System.getenv("FLUTTER_ROOT")
+            require(sdkPath != null) { "Flutter SDK not found. Define flutter.sdk in local.properties or FLUTTER_ROOT env variable" }
+            sdkPath
         }
 
     includeBuild("$flutterSdkPath/packages/flutter_tools/gradle")
